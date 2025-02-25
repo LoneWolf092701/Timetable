5cc978662e34361e3e13c15d16814a645fd5e3ea
Kaladaran <124386704+LoneWolf092701@users.noreply.github.com>
2025-02-25 20:24:34 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const cleaners = [
+    "Kaladaran",
+    "Kabishan",
+    "Lucky",
+    "Niru",
+    "Hobishan",
+    "Titus",
+];
+
+const today = new Date().getDay(); // 0 (Sunday) - 6 (Saturday)
+document.getElementById("cleaner-name").textContent = cleaners[today];
